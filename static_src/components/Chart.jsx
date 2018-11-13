b920df87b32078703c0234e95f905d23d0f02b6b
Lizanoskova <enoskovamipt@gmail.com>
2018-11-13 21:06:44 +0300

    add other choices

--- static_src/components/Chart.jsx
+++ static_src/components/Chart.jsx
@@ -25,15 +25,6 @@ import { bindActionCreators } from 'redux';
 import store from './../index.jsx';
 import { getJSON, RSAA } from 'redux-api-middleware';
 
-// const data = [
-//     {id: '1', Электрик: 4, Сантехник: 5, Плотник: 5},
-//     {id: '2', Электрик: 2, Сантехник: 1, Плотник: 8},
-//     {id: '3', Электрик: 6, Сантехник: 8, Плотник: 13},
-//     {id: '4', Электрик: 9, Сантехник: 9, Плотник: 9},
-//     {id: '5', Электрик: 3, Сантехник: 12, Плотник: 6},
-// ];
-
-
 const styles = theme => ({
     button: {
         maxHeight: 20,
@@ -89,6 +80,7 @@ class Chart extends React.Component {
         clickedElictrician: false,
         clickedPlumber: false,
         clickedChemistry: false,
+        clickedOther: false,
         colorProcessing: 'default',
         colorSent: 'default',
         colorUrgent: 'default',
@@ -96,6 +88,7 @@ class Chart extends React.Component {
         colorElictrician: 'default',
         colorPlumber: 'default',
         colorChemistry: 'default',
+        colorOther: 'default',
         fromDate: '',
         toDate: '',
         status:'',
@@ -144,6 +137,12 @@ class Chart extends React.Component {
         this.setState({ clickedChemistry: ! this.state.clickedChemistry });
         this.setState({ colorChemistry: this.state.clickedChemistry ? 'default' : 'secondary' });
         this.setState({ category: 'Хим обработка' });
+    };
+
+    searchOther = event => {
+        this.setState({ clickedOther: ! this.state.clickedOther });
+        this.setState({ colorOther: this.state.clickedOther ? 'default' : 'secondary' });
+        this.setState({ category: 'Другое' });
 
     };
     handleChange = name => event => {
@@ -151,26 +150,6 @@ class Chart extends React.Component {
           [name]: event.target.value,
         });
       };
-    // drawChart = event => {
-    //     console.log(apiUrls.filter( this.state.status,
-    //                                 this.state.category,
-    //                                 this.state.urgency,
-    //                                 this.state.fromDate, 
-    //                                 this.state.toDate));
-    //     this.props.filterRequest(apiUrls.filter(this.state.status,
-    //                                                         this.state.category,
-    //                                                         this.state.urgency,
-    //                                                         this.state.fromDate, 
-    //                                                         this.state.toDate),
-    //                                                         store.getState().auth.token).then(
-    //                                                            (json) => {
-    //                                                                 // console.log(json.payload.entities.requests);
-    //                                                                 const data = functions.formDataSet(json.payload.entities.requests);
-    //                                                                 this.setState({ Data: data });
-    //                                                             }
-    //                                                         );
-    
-    // };
 
     drawChart = event => {
         this.props.prepareData(apiUrls.filter(  this.state.status,
@@ -182,8 +161,6 @@ class Chart extends React.Component {
     };
     render() {
         const {classes} = this.props;
-        console.log(this.state.Data);
-        //console.log(data);
         return (
             <div>
             <NavBar/>
@@ -294,7 +271,18 @@ class Chart extends React.Component {
                     >
                     ХИМ ОБРАБОТКА
                 </Button>
-                <Button variant="contained" className={classes.button} >
+                <Button variant="contained" className={classes.button}
+                    style={
+                        this.state.colorOther === 'secondary'
+                            ? {
+                            '--background-start': '#ec407a',
+                            }
+                            : {
+                            '--background-end': '#ffffff',
+                            }
+                        }
+                        onClick={this.searchOther} 
+                        >
                     ДРУГОЕ
                 </Button>
                 <Typography
@@ -344,6 +332,7 @@ class Chart extends React.Component {
                         <Legend/>
                         <Line type="monotone" dataKey="Электрик" stroke="#82ca9d"/>
                         <Line type="monotone" dataKey="Сантехник" stroke="#ff1744"/>
+                        <Line type="monotone" dataKey="Другое" stroke="#ff8000"/>
                         <Line type="monotone" dataKey="Плотник" stroke="#8884d8" activeDot={{r: 8}}/>
                     </LineChart>
                 </ResponsiveContainer>
@@ -359,11 +348,6 @@ Chart.propTypes = {
     classes: PropTypes.object.isRequired,
 };
 
-// const mapStateToProps = (state) => {
-//     return {
-//         data: state.data,
-//     }
-// }
 function mapStateToProps (state) {
     console.log('State:');
     console.log(state);
